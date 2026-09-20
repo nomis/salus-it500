da81a5aee6864775492bda39e6bb80e652016ae6
Simon Arlott <sa.me.uk>
2026-09-20 11:04:18 +0100

    Automatically repeat status information and log it to syslog

--- salus-influxdb.py
+++ salus-influxdb.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import argparse
 import codecs
+import os
 import socket
 import struct
 import sys
+import syslog
+import time
 import traceback
+
+import yaml
 
 socket.MCAST_JOIN_SOURCE_GROUP = 46
 
@@ -13,7 +18,8 @@ DST_PORT = 19541
 GROUP = "ff02::114"
 
 class Salus:
-	def __init__(self, interface, ip):
+	def __init__(self, interface, ip, state_file):
+		self.state_file = state_file
 		self.hostname = socket.getfqdn()
 		ifidx = socket.if_nametoindex(interface)
 
@@ -41,6 +47,21 @@ class Salus:
 		self.influx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		self.influx.connect(("127.0.0.1", 8089))
 
+		self.previous_ping_ts = None
+		self.load_state()
+
+	def load_state(self):
+		try:
+			with open(self.state_file, "r") as f:
+				self.state = yaml.safe_load(f)
+		except FileNotFoundError:
+			self.state = {}
+
+	def save_state(self):
+		with open(f"{self.state_file}~", "w") as f:
+			yaml.dump(self.state, f)
+		os.rename(f"{self.state_file}~", self.state_file)
+
 	def receive(self):
 		while True:
 			(packet, address) = self.sock.recvfrom(65536)
@@ -58,6 +79,9 @@ class Salus:
 			except Exception:
 				print(codecs.encode(packet[13:], "hex"), file=sys.stderr)
 				traceback.print_exc()
+				syslog.syslog(codecs.encode(packet[13:], "hex"))
+				for line in traceback.format_exc().split("\n"):
+					syslog.syslog(line)
 
 	def decode(self, ts, incoming, data):
 		cmd = (data[0] << 8) | data[1]
@@ -93,12 +117,24 @@ class Salus:
 	def process(self, ts, incoming, data):
 		print(f"{ts:.6f}", "->" if incoming else "<-", f"{data['cmd']:04x}", data["name"], data["data"])
 
-		ts = int(ts * 1000000000)
-
 		if incoming and data["name"] == "status":
-			#print(hdr.getts(), data["tlv"])
-			self.send(f"temperature,location=home,host={self.hostname},sensor=external:salus,traits=metric:gauge celsius={data['tlv'][0x1b]/100},target:celsius={data['tlv'][0x1c]/100} {ts}".encode("utf-8"))
-			self.send(f"boiler,location=home,host={self.hostname},sensor=external:salus,traits=metric:gauge,salus:ch=power active={data['tlv'][0x1e]} {ts}".encode("utf-8"))
+			print(f"{ts:.6f}", data["tlv"])
+			if time.time() - ts < 10:
+			    syslog.syslog(f"{ts:.6f} {data['name']} {data['tlv']}")
+			self.send_status(ts, data)
+			self.state["status_data"] = [ts, data]
+			self.save_state()
+			self.previous_ping_ts = None
+		elif incoming and data["name"] == "ping":
+			if self.previous_ping_ts is not None and "status_data" in self.state:
+				if time.time() - self.state["status_data"][0] < 86400:
+					self.send_status(self.previous_ping_ts, self.state["status_data"][1])
+			self.previous_ping_ts = ts
+
+	def send_status(self, ts, data):
+		ts = int(ts * 1000000000)
+		self.send(f"temperature,location=home,host={self.hostname},sensor=external:salus,traits=metric:gauge celsius={data['tlv'][0x1b]/100},target:celsius={data['tlv'][0x1c]/100} {ts}".encode("utf-8"))
+		self.send(f"boiler,location=home,host={self.hostname},sensor=external:salus,traits=metric:gauge,salus:ch=power active={data['tlv'][0x1e]} {ts}".encode("utf-8"))
 
 	def send(self, message):
 		print(message)
@@ -108,5 +144,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("interface")
 	parser.add_argument("ip")
+	parser.add_argument("state_file")
 	args = parser.parse_args()
+	syslog.openlog("salus-influxdb")
 	Salus(**vars(args)).receive()
